75ca1f769d47e5fc5d116d48ad7e73d2442211b4
JustAHedgehog <143854000+JustAHedgehog@users.noreply.github.com>
2025-05-14 14:41:25 +0800

    Add files via upload

---
+++ week_12/Week12_隨堂練習1_110303047.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('rice-shaded.tif')
+edges = cv2.Canny(img,100,200)
+
+# 將邊緣資訊重疊到原始圖片上
+# 建立一個與原始圖片大小相同的全黑圖片
+contour_img = np.zeros_like(img)
+
+# 將 Canny 偵測到的邊緣 (白色像素) 複製到 contour_img 上
+contour_img[edges != 0] = (0, 0, 255)  # 使用紅色 (0, 0, 255) 標記邊緣
+
+# 將 contour_img 與原始圖片 img 重疊
+# result = cv2.addWeighted(img, 1, contour_img, 0.5, 0)
+result = cv2.add(img, contour_img)
+
+# 顯示結果
+cv2.imshow(result)
